b406dd20a37876767308510da619b88bde943c45
z9877969 <z9877969@ukr.net>
2022-05-24 17:37:18 +0300

    removes comments into l-19

--- lesson-19(restricted-routes)/code/src/pages/AuthPage.jsx
+++ lesson-19(restricted-routes)/code/src/pages/AuthPage.jsx
@@ -1,47 +1,9 @@
 import { useDispatch } from "react-redux";
 import { Navigate, useParams } from "react-router-dom";
 import AuthForm from "../components/AuthForm/AuthForm";
-import Form from "../components/Form/Form";
 import LoginForm from "../components/LoginForm/LoginForm";
 import RegisterForm from "../components/RegisterForm/RegisterForm";
 import { loginUser, registerUser } from "../redux/auth/authOperations";
-
-// const loginFormOptions = [
-//   {
-//     title: "Email",
-//     name: "email",
-//   },
-//   {
-//     title: "Password",
-//     name: "password",
-//   },
-// ];
-
-// const loginInitialValues = {
-//   email: "",
-//   password: "",
-// };
-
-// const registerFormOptions = [
-//   {
-//     title: "Email",
-//     name: "email",
-//   },
-//   {
-//     title: "Password",
-//     name: "password",
-//   },
-//   {
-//     title: "Confirm",
-//     name: "confirm",
-//   },
-// ];
-
-// const registerInitialValues = {
-//   email: "",
-//   password: "",
-//   confirm: "",
-// };
 
 const AuthPage = () => {
   const dispatch = useDispatch();
